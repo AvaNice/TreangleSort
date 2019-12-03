4b5703d9252aeb684c6e01926abd8afbc47fbf66
Alexandr Globa <globaalexandr@gmail.com>
2019-12-03 13:39:21 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreangleSort
+{
+    class Program
+    {
+        static List<Treangle> treangles = new List<Treangle>();
+
+        static void Main()
+        {
+            treangles.Add(GetTreangle());
+
+            treangles.Sort(new TreangleComparer());
+            foreach (var item in treangles)
+            {
+                Console.WriteLine($"[{item.Name}] {item.Area} ");
+            }
+        }
+        static Treangle GetTreangle()
+        {
+            string input = Console.ReadLine();
+
+            string[] splitedInput = new string[4];
+
+            splitedInput = input.Split(',');
+
+            string name;
+            double firstSide;
+            double secondSide;
+            double thirdSide;
+
+            name = splitedInput[0];
+            firstSide = Convert.ToDouble(splitedInput[1]);
+            secondSide = Convert.ToDouble(splitedInput[2]);
+            thirdSide = Convert.ToDouble(splitedInput[3]);
+
+            Treangle treangle = new Treangle(name, firstSide, secondSide, thirdSide);
+
+            if (treangle.Area > 0)
+            {
+                return treangle;
+            }
+
+            throw new Exception("Sides of the triangle entered incorrectly!");
+
+        }
+    }
+}
---
+++ Treangle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreangleSort
+{
+    class Treangle
+    {
+        public string Name { get; }
+        public double Area { get; }
+
+        public Treangle(string name, double firstSide, double secondSide, double thirdSide)
+        {
+            Name = name;
+            Area = TreangleAreaCounter.CountArea(firstSide, secondSide, thirdSide);
+        }
+    }
+}
