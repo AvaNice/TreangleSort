396bb817ffa3976746b4794924e97489ae766a3a
Alexandr Globa <globaalexandr@gmail.com>
2019-12-20 15:19:41 +0200

    Tests

---
+++ TriangleSortTests/TriangleTests.cs
@@ -0,0 +1,72 @@
+﻿using TreangleSort;
+using Xunit;
+
+namespace TriangleSortTests
+{
+    public class TriangleTests
+    {
+        [Theory]
+        [InlineData(3, 4, 5, 12)]
+        [InlineData(12, 14, 13, 39)]
+        [InlineData(4, 6, 9, 19)]
+        public void PerimeterTest(double firstSide, double secondSide, double thirdSide, double expected)
+        {
+            Triangle triangle = new Triangle("Test", firstSide, secondSide, thirdSide);
+
+            Assert.Equal(triangle.Perimeter, expected);
+        }
+
+        [Theory]
+        [InlineData(3, 4, 5, 6)]
+        [InlineData(12, 14, 13, 72.307935)]
+        [InlineData(4, 6, 9, 9.562296)]
+        public void AreaTest(double firstSide, double secondSide, double thirdSide, double expected)
+        {
+            Triangle triangle = new Triangle("Test", firstSide, secondSide, thirdSide);
+
+            Assert.Equal(triangle.Area, expected, 6);
+        }
+
+        [Fact]
+        public void CompareTestTriangleArea6ToTreangleArea12Expect1()
+        {
+            int result;
+            int expected = 1;
+           
+            Triangle first = new Triangle("Test", 3, 4, 5);
+            Triangle second = new Triangle("Test", 5, 5, 6);
+
+            result = first.CompareTo(second);
+
+            Assert.Equal(result, expected);
+        }
+
+        [Fact]
+        public void CompareTestTriangleArea6ToTreangleArea6Expect0()
+        {
+            int result;
+            int expected = 0;
+
+            Triangle first = new Triangle("Test", 3, 4, 5);
+            Triangle second = new Triangle("Test", 4, 3, 5);
+
+            result = first.CompareTo(second);
+
+            Assert.Equal(result, expected);
+        }
+
+        [Fact]
+        public void CompareTestTriangleArea12ToTreangleArea6ExpectMinus1()
+        {
+            int result;
+            int expected = -1;
+
+            Triangle first = new Triangle("Test", 5, 5, 6);
+            Triangle second = new Triangle("Test", 4, 3, 5);
+
+            result = first.CompareTo(second);
+
+            Assert.Equal(result, expected);
+        }
+    }
+}
---
+++ TriangleSortTests/TriangleValidatorTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TreangleSort;
+using Xunit;
+
+namespace TriangleSortTests
+{
+    public class TriangleValidatorTests
+    {
+        [Theory]
+        [InlineData(321, 4, 5)]
+        [InlineData(-5, -5, -5)]
+        [InlineData(0, 0, 0)]
+        public void ValidatorWrongSideExpectFalse(double firstSide, double secondSide, double thirdSide)
+        {          
+            TriangleValidator validator = new TriangleValidator();
+            Triangle triangle = new Triangle("Test", firstSide, secondSide, thirdSide);
+
+            Assert.False(validator.Validate(triangle).IsValid);
+        }
+
+        [Theory]
+        [InlineData(3, 4, 5)]
+        [InlineData(7, 17, 16)]
+        [InlineData(11, 17.5, 7)]
+        public void ValidatorRightSideExpectTrue(double firstSide, double secondSide, double thirdSide)
+        {
+            TriangleValidator validator = new TriangleValidator();
+            Triangle triangle = new Triangle("Test", firstSide, secondSide, thirdSide);
+
+            Assert.True(validator.Validate(triangle).IsValid);
+        }
+    }
+}
